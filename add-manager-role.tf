e2e5b3dbac1b5588626097c4c228615b122751a6
kilik42 <marvinevins@yahoo.com>
2025-09-06 16:43:31 -0500

    adding add manager role

--- add-manager-role.tf
+++ add-manager-role.tf
@@ -54,3 +54,28 @@ resource "aws_iam_policy" "eks_admin"{
         ]
     })
 }
+
+resource "aws_iam_role_policy_attachment" "eks_admin_attach" {
+  role       = aws_iam_role.eks_admin.name
+  policy_arn = aws_iam_policy.eks_admin.arn
+}
+
+resource "aws_iam_user" "manager" {
+  name = "manager"
+}
+
+resource "aws_iam_policy" "eks_assume_policy" {
+  name        = "${local.env}-${local.eks_name}-assume-role-policy"
+  description = "Policy to allow user to assume the EKS manager role"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Resource = aws_iam_role.eks_admin.arn
+      },
+    ]
+  })
+}

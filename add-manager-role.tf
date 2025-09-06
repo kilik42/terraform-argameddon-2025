a0687b449b9be3e8325d2f4d86e44263343c15df
kilik42 <marvinevins@yahoo.com>
2025-09-06 16:30:32 -0500

    added i am policy

--- add-manager-role.tf
+++ add-manager-role.tf
@@ -22,3 +22,35 @@ resource "aws_iam_role" "eks_admin" {
   })
   
 }
+
+# Attach the AmazonEKSClusterPolicy to the role
+resource "aws_iam_policy" "eks_admin"{
+    name = "${local.env}-${local.eks_name}-manager-policy"
+    
+    description = "Policy to allow manager role to access EKS cluster"
+    
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+        {
+            Effect = "Allow"
+            Action = [
+            "eks:ListClusters",
+            "eks:DescribeCluster",
+            "eks:ListNodegroups",
+            "eks:DescribeNodegroup",
+            "eks:ListFargateProfiles",
+            "eks:DescribeFargateProfile",
+            "eks:ListUpdates",
+            "eks:DescribeUpdate",
+            "eks:ListAddons",
+            "eks:DescribeAddon",
+            "eks:ListIdentityProviderConfigs",
+            "eks:DescribeIdentityProviderConfig",
+            "eks:AccessKubernetesApi"
+            ]
+            Resource = "*"
+        },
+        ]
+    })
+}

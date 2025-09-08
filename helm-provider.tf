4835f91cb0a4526d743e9164a6619723fef4e2f9
kilik42 <marvinevins@yahoo.com>
2025-09-08 05:20:18 -0500

    working on helm provider

---
+++ helm-provider.tf
@@ -0,0 +1,19 @@
+
+
+# we need to get the cluster details and auth info from the EKS cluster we created
+# this is needed to configure the helm provider to connect to the EKS cluster
+data "aws_eks_cluster" "eks" {
+  name = aws_eks_cluster.eks.name
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = aws_eks_cluster.eks.name
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
